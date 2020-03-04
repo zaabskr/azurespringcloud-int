d03a31935f627083faf6188a76d60127804655fb
zaabskr <52285170+zaabskr@users.noreply.github.com>
2020-03-04 16:11:59 +0530

    Update automate_docker_install.sh

--- automate_docker_install.sh
+++ automate_docker_install.sh
@@ -1,4 +1,17 @@
-apt-get install clamav clamav-daemon -y
 systemctl stop clamav-freshclam
-freshclam
-systemctl start clamav-freshclam
+apt-get remove clamav clamav-daemon -y
+apt autoremove
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl status docker
+sudo usermod -aG docker delladmin
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker images
+mkdir intake-service
+sudo docker-compose --version
+sudo docker ps
